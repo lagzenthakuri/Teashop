52b550b98981cb1dfcc63d4665b715e7e4737226
lagzenthakuri <thakurizen2@gmail.com>
2025-11-27 10:39:17 +0545

    changing color code and adding logo

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -1,13 +1,13 @@
 tailwind.config = {
     theme: {
         extend: {
-            colors: {
-                'primary-bg': '#E6F1EA',       // Soft but richer green background
-                'primary-text': '#1B3B2D',     // Dark Forest Green for text
-                'accent': '#6B8E23',           // Olive Green as accent
-                'promo': '#388E3C',            // Deep Vibrant Green for promotions
-                'secondary': '#CFE8D6',        // Subtle leafy green for secondary elements
-            },
+            colors: { 'primary-bg': '#E6F1EA', 
+             'primary-text': '#1B3B2D', 
+              'accent': '#143233',
+              'promo': '#2A5859',
+              'secondary': '#CFE8D6',
+             },
+
 
 
             fontFamily: {
